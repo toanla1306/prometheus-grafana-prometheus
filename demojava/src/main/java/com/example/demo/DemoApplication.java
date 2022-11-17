fa0397b3bb0d73ae60e3231c3649f60809c38baf
toan la <ldtoan1306@gmail.com>
2022-11-17 09:00:25 +0700

    update code

--- demojava/src/main/java/com/example/demo/DemoApplication.java
+++ demojava/src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		// PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+        // try {
+        //     HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        //     server.createContext("/prometheus", httpExchange -> {
+        //         String response = prometheusRegistry.scrape();
+        //         httpExchange.sendResponseHeaders(200, response.getBytes().length);
+        //         try (OutputStream os = httpExchange.getResponseBody()) {
+        //             os.write(response.getBytes());
+        //         }
+        //     });
+        //     new Thread(server::start).start();
+        // } catch (IOException e) {
+        //     throw new RuntimeException(e);
+        // }
 		SpringApplication.run(DemoApplication.class, args);
 		
 	}
--- demojava/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ demojava/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -11,36 +11,8 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("api")
 public class HelloWorldController {
 
-    @GetMapping(value = "/hello")
-    public ResponseEntity<?> demo() {
-        try {       
-            System.out.println('hello world');
-            abc();
-            return ResponseEntity.ok(null);
-        } catch (Exception e) {
-            System.out.println(e);
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.getReasonPhrase(), e);
-        }
+    @GetMapping("/test") // Nếu người dùng request tới địa chỉ "/"
+    public String index() {
+        return "index"; // Trả về file index.html
     }
-
-    
-    public void abc () {
-        PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
-
-try {
-    HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-    server.createContext("/prometheus", httpExchange -> {
-        String response = prometheusRegistry.scrape(); (1)
-        httpExchange.sendResponseHeaders(200, response.getBytes().length);
-        try (OutputStream os = httpExchange.getResponseBody()) {
-            os.write(response.getBytes());
-        }
-    });
-
-    new Thread(server::start).start();
-} catch (IOException e) {
-    throw new RuntimeException(e);
-}
-    }
-
 }
